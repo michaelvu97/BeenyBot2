890bcf65bc46c6b7acfe5a5bf663a1dcd6759009
Michael Vu <michael.vu.97@outlook.com>
2020-01-25 12:33:04 -0500

    Added main.py stub

---
+++ src/main.py
@@ -0,0 +1,28 @@
+#
+# Read image
+#
+
+#
+# Run OCR
+#
+
+#
+# Fuzzy match with products, get list of most likely products
+#
+
+#
+# Do OCR and get positional data
+#
+
+#
+# Determine the position origin of each product
+#
+
+#
+# Determine the other labels for each product
+#
+
+
+#
+# Output to csv
+#
